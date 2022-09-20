05e4fcaef78996be620a24edda7a2566c54357f6
TejasSC <tsctejas@gmail.com>
2022-09-20 18:11:21 +0800

    drop rows with null values

--- netflix-recommender.py
+++ netflix-recommender.py
@@ -4,5 +4,8 @@ from sklearn.feature_extraction import text
 from sklearn.metrics.pairwise import cosine_similarity
 
 data = pd.read_csv("netflixData.csv")
-#print(data.head())
-print(data.isnull().sum())
+
+# drop rows containing null values 
+data = data.dropna()
+print(data.head()) #checking the first 5 rows of data
+print(data.isnull().sum()) #checking for null values in each column 
