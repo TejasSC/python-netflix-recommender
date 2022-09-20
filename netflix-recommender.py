cd5df5b8c7f067b786cfa6f54ef489dde3fc14c0
TejasSC <tsctejas@gmail.com>
2022-09-20 18:18:16 +0800

    clean Title column to get essential words only

--- netflix-recommender.py
+++ netflix-recommender.py
@@ -2,10 +2,34 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction import text
 from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+import re
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
 
 data = pd.read_csv("netflixData.csv")
-
+print(data.head()) #checking the first 5 rows of data
 # drop rows containing null values 
 data = data.dropna()
-print(data.head()) #checking the first 5 rows of data
-print(data.isnull().sum()) #checking for null values in each column 
+
+# clean function to remove stopwords, unnecessary punctuation, non-alphanumeric characters etc. 
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+
+data["Title"] = data["Title"].apply(clean) #use clean function on Title column of movies 
+
+print(data.head()) 
